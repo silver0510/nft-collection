a36bf012f1386c2d7dc8b3cf13ffc1019bc1689e
nghiapp7018 <nghia.pham@co-well.vn>
2022-12-27 05:52:41 +0700

    update contract add

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x87b6843bb10310778a57c2d47a930afdc64d5d26";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-three-delta.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
